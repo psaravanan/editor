e239876f98c26a55fdaa1679c9b5376678737ebd
saravanan <psaravanan11@gmail.com>
2013-04-18 14:54:57 +0530

    editor added successfully

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,19 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require tinymce-jquery
 //= require_tree .
+
+tinyMCE.init({
+		theme : "advanced",
+		plugins: "table",
+    style_formats : [
+        {title : 'Bold text', inline : 'b'},
+        {title : 'Red text', inline : 'span', styles : {color : '#ff0000'}},
+        {title : 'Red header', block : 'h1', styles : {color : '#ff0000'}},
+        {title : 'Example 1', inline : 'span', classes : 'example1'},
+        {title : 'Example 2', inline : 'span', classes : 'example2'},
+        {title : 'Table styles'},
+        {title : 'Table row 1', selector : 'tr', classes : 'tablerow1'}
+    ]
+});
